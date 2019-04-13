dda5928152b3b3d104b67863f8b61fe6a6293367
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-04-13 14:32:22 +0900

    Hello Android

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.kt
@@ -0,0 +1,28 @@
+package jp.ac.titech.itpro.sdl.helloandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var outputView: TextView
+    private lateinit var inputName: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        outputView = findViewById(R.id.output_view)
+        inputName = findViewById(R.id.input_name)
+    }
+
+    fun greet(v: View) {
+        val name = inputName.text.toString().trim()
+        if (name.isNotEmpty()) {
+            outputView.text = "Hello $name!\nNice to see you!"
+        }
+    }
+
+}
